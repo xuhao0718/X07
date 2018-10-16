e02873fe8b1f81de7183579624eb036128eab334
Xuhao <13271272515@163.com>
2018-10-16 10:00:44 +0800

    Add HttpServer

---
+++ HttpServer/WebFram/setting.py
@@ -0,0 +1,8 @@
+# WebFrame设置
+
+frame_ip = '127.0.0.1'
+frame_port = 8080
+farme_addr = (frame_ip,frame_port)
+
+# 静态文件储存路径
+STATIC_DIR = './static'
---
+++ HttpServer/WebFram/views.py
@@ -0,0 +1,11 @@
+from time import ctime
+
+
+def show_time():
+    return ctime()
+
+def say_hello():
+    return "Heelo Python"
+
+def say_bye():
+    return "Good bye"
